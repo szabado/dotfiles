5a43c9af22b5afa955b5a806bf22dcedc50e1d57
Felix Jancso-Szabo <felix.ajs@gmail.com>
2018-06-01 01:25:12 -0400

    patched some bugs

--- ubuntu/set-defaults.sh
+++ ubuntu/set-defaults.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-ln -s ${HOME}/.dotfiles/ubuntu/schemas/org.gnome.shell.extensions.user-theme.gschema.xml /usr/share/glib-2.0/schemas/
-glib-compile-schemas /usr/share/glib-2.0/schemas
+set -xe
+sudo ln -s ${HOME}/.dotfiles/ubuntu/schemas/org.gnome.shell.extensions.user-theme.gschema.xml /usr/share/glib-2.0/schemas/
+sudo glib-compile-schemas /usr/share/glib-2.0/schemas
 dconf load ${HOME}/.dotfiles/ubuntu/
 dconf load /org/gnome/shell/extensions/ < ${HOME}/.dotfiles/ubuntu/theme_dconf.txt
 sudo shutdown -r 0
