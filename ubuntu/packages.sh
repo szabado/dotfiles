17dd0bc4ca31d423df21a4d3c67e74b0c0558953
Felix Jancso-Szabo <felix.ajs@gmail.com>
2018-06-08 00:16:31 -0400

    Added checks to not reinstall packages

--- ubuntu/packages.sh
+++ ubuntu/packages.sh
@@ -36,16 +36,20 @@ installx xclip
 installx vlc
 
 # install google chrome
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub 2>/dev/null | sudo apt-key add -  > /dev/null 2>&1
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' > /dev/null 2>&1
-sudo apt-get update > /dev/null 2>&1
+if [[ -z "$(which google-chrome-stable)" ]]; then
+	wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub 2>/dev/null | sudo apt-key add -  > /dev/null 2>&1
+	sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' > /dev/null 2>&1
+	sudo apt-get update > /dev/null 2>&1
+fi
 installx google-chrome-stable
 
 # install sublime text
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg 2>/dev/null | sudo apt-key add -  > /dev/null 2>&1
-installx apt-transport-https
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list > /dev/null 2>&1
-sudo apt-get update > /dev/null 2>&1
+if [[ -z "$(which subl)" ]]; then
+	wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg 2>/dev/null | sudo apt-key add -  > /dev/null 2>&1
+	installx apt-transport-https
+	echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list > /dev/null 2>&1
+	sudo apt-get update > /dev/null 2>&1
+fi
 installx sublime-text
 
 # install calibre
@@ -53,7 +57,10 @@ installx sublime-text
 success "Installed calibre"
 
 # install ripgrep
-curl -LO https://github.com/BurntSushi/ripgrep/releases/download/0.8.1/ripgrep_0.8.1_amd64.deb > /dev/null 2>&1
-sudo dpkg -i ripgrep_0.8.1_amd64.deb > /dev/null 2>&1
+if [[ -z "$(which ripgrep)" ]]; then
+	curl -LO https://github.com/BurntSushi/ripgrep/releases/download/0.8.1/ripgrep_0.8.1_amd64.deb > /dev/null 2>&1
+	sudo dpkg -i ripgrep_0.8.1_amd64.deb > /dev/null 2>&1
+	rm -f ripgrep_0.8.1_amd64.deb > /dev/null 2>&1
+fi
 success "Installed ripgrep"
 
