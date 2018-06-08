945adcbfff5ec1245806792928845b1a4e77a584
Felix Jancso-Szabo <felix.ajs@gmail.com>
2018-06-08 01:12:22 -0400

    Changed git to use a bootstrap script instead of the hardcoded config

--- git/bootstrap.sh
+++ git/bootstrap.sh
@@ -1,20 +1,31 @@
 #!/usr/bin/env bash
+#
+# Configure git
 
 source install/helpers
+
+set -e
 
 DOTFILES_ROOT="$(pwd)"
 
 lnx "${DOTFILES_ROOT}/git/gitignore_global" "${HOME}/.gitignore_global"
 
-git config --global user.name "Felix Jancso-Szabo"
+if [[ -z "$(git config --get --global user.name)" ]]; then
+	user 'What is your github nom de plume?'
+	read -e git_name
+	git config --global user.name "${git_name}"
+fi
 if [[ -z "$(git config --get --global user.email)" ]]; then
 	user 'What is your github email?'
 	read -e git_email
 	git config --global user.email "${git_email}"
 fi
-# git config --global url.git@github.com:.insteadOf https://github.com
+if prompt "Disable https for git in favour of ssh?"; then
+	git config --global url.git@github.com:.insteadOf https://github.com
+fi
 git config --global core.exludesfile ~/.gitignore_global
 git config --global push.default simple
 git config --global core.editor $(which vim)
 
 success "git config complete"
+
