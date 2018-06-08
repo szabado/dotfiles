62eec0cc3b1a790cb0f90916713b9272177cd442
Felix Jancso-Szabo <felix.ajs@gmail.com>
2018-06-07 21:25:58 -0400

    Made apt-get installation cleaner

--- ubuntu/packages.sh
+++ ubuntu/packages.sh
@@ -2,40 +2,49 @@
 #
 # The equivalent to the macos brewfile
 
+set -e
+
+installx() {
+	installx ${@} > /dev/null && success "Installed ${*}"
+}
+
 sudo apt-get --assume-yes update
 sudo apt-get --assume-yes upgrade
 
 # install the *required* things first
-sudo apt-get --assume-yes install git-core
-sudo apt-get --assume-yes install vim
-sudo apt-get --assume-yes install zsh
-wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+installx git-core
+installx vim
+installx zsh
+# Install oh-my-zsh
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh > /dev/null && success "Installed oh-my-zsh"
 
-sudo apt-get --assume-yes install arc-theme
-sudo apt-get --assume-yes install colordiff
-sudo apt-get --assume-yes install dconf-tools
-sudo apt-get --assume-yes install deluge
-sudo apt-get --assume-yes install firefox
-sudo apt-get --assume-yes install google-chrome-stable libappindicator1 
-sudo apt-get --assume-yes install lm-sensors powertop tlp tlp-rdw 
-sudo apt-get --assume-yes install screen
-sudo apt-get --assume-yes install sshfs
-sudo apt-get --assume-yes install tmux
-sudo apt-get --assume-yes install tree
-sudo apt-get --assume-yes install xclip
-sudo apt-get --assume-yes install vlc
+installx arc-theme
+installx colordiff
+installx curl
+installx dconf-tools
+installx deluge
+installx firefox
+installx google-chrome-stable libappindicator1
+installx lm-sensors powertop tlp tlp-rdw
+installx screen
+installx sshfs
+installx tmux
+installx tree
+installx xclip
+installx vlc
 
 # install sublime text
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-sudo apt-get --assume-yes install apt-transport-https
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt-get --assume-yes update
-sudo apt-get --assume-yes install sublime-text
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add - > /dev/null
+installx apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list > /dev/null
+installx sublime-text
+success "Installed sublime text"
 
 # install calibre
-sudo -v && wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sudo sh /dev/stdin
+sudo -v && wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sudo sh /dev/stdin > /dev/null && success "Installed calibre"
 
 # install ripgrep
-curl -LO https://github.com/BurntSushi/ripgrep/releases/download/0.8.1/ripgrep_0.8.1_amd64.deb
-sudo dpkg -i ripgrep_0.8.1_amd64.deb
+curl -LO https://github.com/BurntSushi/ripgrep/releases/download/0.8.1/ripgrep_0.8.1_amd64.deb > /dev/null
+sudo dpkg -i ripgrep_0.8.1_amd64.deb > /dev/null
+success "Installed ripgrep"
 
