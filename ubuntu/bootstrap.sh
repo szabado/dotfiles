9cb267027762baa9730ad641c6f6bfe997e777de
Felix Jancso-Szabo <felix.ajs@gmail.com>
2018-06-13 22:39:13 -0400

    added set-defaults contents to bootstrap for ubuntu

--- ubuntu/bootstrap.sh
+++ ubuntu/bootstrap.sh
@@ -2,5 +2,9 @@
 #
 # Set up ubuntu
 
+set -e
 gsettings set org.gnome.desktop.background picture-uri file://${HOME}/.dotfiles/pics/Background.jpg
+sudo ln -s ${HOME}/.dotfiles/ubuntu/schemas/org.gnome.shell.extensions.user-theme.gschema.xml /usr/share/glib-2.0/schemas/
+sudo glib-compile-schemas /usr/share/glib-2.0/schemas
+dconf load /org/gnome/shell/extensions/ < ${HOME}/.dotfiles/ubuntu/theme_dconf.txt
 
--- ubuntu/set-defaults.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -xe
-sudo ln -s ${HOME}/.dotfiles/ubuntu/schemas/org.gnome.shell.extensions.user-theme.gschema.xml /usr/share/glib-2.0/schemas/
-sudo glib-compile-schemas /usr/share/glib-2.0/schemas
-dconf load /org/gnome/shell/extensions/ < ${HOME}/.dotfiles/ubuntu/theme_dconf.txt
-sudo shutdown -r 0
