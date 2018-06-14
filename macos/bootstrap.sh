a3a38e2e583a21b8829791a8c6b1eb66ce7df5f2
Felix Jancso-Szabo <felix.ajs@gmail.com>
2018-06-13 22:02:05 -0400

    Added commands to set wallpaper

---
+++ macos/bootstrap.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# Commented out because I'm not *sure* this works, but it **should**  set the bacground image properly
+# sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "update data set value = '${HOME}/.dotfiles/pics/Background.jpg'";
+# killall Dock;
---
+++ ubuntu/bootstrap.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+#
+# Set up ubuntu
+
+gsettings set org.gnome.desktop.background picture-uri file://${HOME}/.dotfiles/pics/Background.jpg
+
