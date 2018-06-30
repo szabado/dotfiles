ba4b58f1909951f2b6a6cd9e70c9be1fe96c0090
Felix Jancso-Szabo <felix.ajs@gmail.com>
2018-06-30 09:16:49 -0400

    Combined all dconf settings into one file
    Combined all of the different individual files into one master file,
    added comments to it.  Also pulled in the gsettings 'set's.

--- ubuntu/bootstrap.sh
+++ ubuntu/bootstrap.sh
@@ -4,13 +4,10 @@
 
 set -e
 gsettings set org.gnome.desktop.background picture-uri file://${HOME}/.dotfiles/pics/Background.jpg
+
 [[ ! -e /usr/share/glib-2.0/schemas/org.gnome.shell.extensions.user-theme.gschema.xml ]] && sudo ln -s ${HOME}/.dotfiles/ubuntu/schemas/org.gnome.shell.extensions.user-theme.gschema.xml /usr/share/glib-2.0/schemas/
 sudo glib-compile-schemas /usr/share/glib-2.0/schemas
-dconf load /org/gnome/shell/extensions/ < ${HOME}/.dotfiles/ubuntu/theme_dconf.txt
 
-gsettings set org.gnome.desktop.interface gtk-theme "Arc-Dark"
-gsettings set org.gnome.desktop.wm.preferences theme "Arc-Dark"
-
-dconf load /org/gnome/terminal/ < ${HOME}/.dotfiles/ubuntu/terminal_config.dconf
-dconf load / < ${HOME}/.dotfiles/ubuntu/favourites.dconf
+# Load the defaults
+cat ./ubuntu/config.dconf | egrep -v "^\s*#.*$" | dconf load /
 
