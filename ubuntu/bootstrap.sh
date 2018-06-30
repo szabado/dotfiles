29823ed97f59405bb5f92e08801b4e174e9189ac
Felix Jancso-Szabo <felix.ajs@gmail.com>
2018-06-30 08:52:01 -0400

    Added manual "User Themes" gnome extension install
    Added some user prompts to install the "User Themes" gnome extension,
    because the theming is currently buggy (likely just because this
    required extension is missing.  It opens the browser to the correct
    page, and tells the user what version to install.  Also added a TODO
    to automate it, with a link to a page with instructions

--- ubuntu/bootstrap.sh
+++ ubuntu/bootstrap.sh
@@ -3,6 +3,20 @@
 # Set up ubuntu
 
 set -e
+
+gnomeVersion="$(cat /usr/share/gnome/gnome-version.xml | egrep "(platform|minor|micro)" | egrep -o "[0-9]+" | paste -sd "." -)"
+
+user "A gnome plugin needs to be downloaded.  Please select the version for Gnome Version ${gnomeVersion}"
+while ! prompt "Are you ready to open the browser?"; do
+	echo "Well GET ready"
+done
+
+# TODO: automate this with bernaerts.dyndns.org/linux/76-gnome/283-gnome-shell-install-extension-command-line-script
+xdg-open "https://extensions.gnome.org/extension/19/user-themes/"
+while ! prompt "Have you installed the plugin yet?"; do
+	echo "Why are you talking to me?? Get busy!!"
+done
+
 gsettings set org.gnome.desktop.background picture-uri file://${HOME}/.dotfiles/pics/Background.jpg
 [[ ! -e /usr/share/glib-2.0/schemas/org.gnome.shell.extensions.user-theme.gschema.xml ]] && sudo ln -s ${HOME}/.dotfiles/ubuntu/schemas/org.gnome.shell.extensions.user-theme.gschema.xml /usr/share/glib-2.0/schemas/
 sudo glib-compile-schemas /usr/share/glib-2.0/schemas
