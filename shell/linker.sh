f873d0ba19b52dec4c1fe274410b51668960b3f0
Felix Jancso-Szabo <jancsoszabo@squareup.com>
2019-10-22 18:06:56 -0400

    patch typo

--- shell/linker.sh
+++ shell/linker.sh
@@ -4,5 +4,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 source "${DIR}/env.sh"
 source "${DIR}/aliases.sh"
-source "${DIR}bash-theme.sh"
+source "${DIR}/bash-theme.sh"
 export PATH="${PATH}:${DIR}/../bin"
