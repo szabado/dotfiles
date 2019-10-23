1620e3718e6d7b8073d1df475877e24555121f29
Felix Jancso-Szabo <jancsoszabo@squareup.com>
2019-10-23 14:28:38 -0400

    Install nerdtree and kotlin vim plugins

--- vim/bootstrap.sh
+++ vim/bootstrap.sh
@@ -14,3 +14,18 @@ mkdir -p ${dst}
 lnx "${vim_configs}/groovy.vim" "${dst}/groovy.vim"
 lnx "${vim_configs}/filetype.vim" "${dst}/filetype.vim"
 lnx "${vim_configs}/scripts.vim" "${dst}/scripts.vim"
+
+running "Installing nerdtree plugin"
+[[ ! -d ~/.vim/pack/vendor/start/nerdtree ]] && git clone https://github.com/scrooloose/nerdtree.git ~/.vim/pack/vendor/start/nerdtree -q
+pushd ~/.vim/pack/vendor/start/nerdtree >/dev/null
+git pull -q
+popd > /dev/null
+vim -u NONE -c "helptags ~/.vim/pack/vendor/start/nerdtree/doc" -c q
+ok
+
+running "Installing kotlin plugin"
+git clone https://github.com/udalov/kotlin-vim.git ~/.vim/pack/plugins/start/kotlin-vim -q
+pushd ~/.vim/pack/plugins/start/kotlin-vim >/dev/null
+git pull -q
+popd > /dev/null
+ok
