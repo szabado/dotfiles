54be1cca89366cb0cc3cdd55e4c0b65b096a9789
Felix Jancso-Szabo <jancsoszabo@squareup.com>
2019-11-05 13:17:26 -0500

    Partial tmux setup, pane integration with vim not working

--- vim/bootstrap.sh
+++ vim/bootstrap.sh
@@ -15,17 +15,52 @@ lnx "${vim_configs}/groovy.vim" "${dst}/groovy.vim"
 lnx "${vim_configs}/filetype.vim" "${dst}/filetype.vim"
 lnx "${vim_configs}/scripts.vim" "${dst}/scripts.vim"
 
-running "Installing nerdtree plugin"
-git clone https://github.com/scrooloose/nerdtree.git ~/.vim/pack/vendor/start/nerdtree -q
-pushd ~/.vim/pack/vendor/start/nerdtree >/dev/null
-git pull -q
-popd > /dev/null
+install_dir="${HOME}/.vim/pack/vendor/start/nerdtree"
+if [[ ! -d "${install_dir}" ]]; then
+	running "Installing nerdtree plugin"
+	git clone https://github.com/scrooloose/nerdtree.git "${install_dir}" -q
+else
+	running "Updating nerdtree plugin"
+	pushd "${install_dir}" >/dev/null
+	git pull -q
+	popd > /dev/null
+fi
 vim -u NONE -c "helptags ~/.vim/pack/vendor/start/nerdtree/doc" -c q
 ok
 
-running "Installing kotlin plugin"
-git clone https://github.com/udalov/kotlin-vim.git ~/.vim/pack/plugins/start/kotlin-vim -q
-pushd ~/.vim/pack/plugins/start/kotlin-vim >/dev/null
-git pull -q
-popd > /dev/null
+install_dir="${HOME}/.vim/pack/plugins/start/kotlin-vim"
+if [[ ! -d "${install_dir}" ]]; then
+	running "Installing kotlin plugin"
+	git clone https://github.com/udalov/kotlin-vim.git "${install_dir}" -q
+else
+	running "Updating kotlin plugin"
+	pushd "${install_dir}" >/dev/null
+	git pull -q
+	popd > /dev/null
+fi
 ok
+
+install_dir="${HOME}/.vim/pack/plugins/start/vim-tmux"
+if [[ ! -d "${install_dir}" ]]; then
+	running "Installing tmux.conf plugin"
+	git clone git://github.com/tmux-plugins/vim-tmux.git "${install_dir}" -q
+else
+	running "Updating tmux.conf plugin"
+	pushd "${install_dir}" >/dev/null
+	git pull -q
+	popd > /dev/null
+fi
+ok
+
+install_dir="${HOME}/.vim/pack/plugins/start/vim-tmux-navigator"
+if [[ ! -d "${install_dir}" ]]; then
+	running "Installing Vim-Tmux navigator plugin"
+	git clone git@github.com:christoomey/vim-tmux-navigator.git "${install_dir}" -q
+else
+	running "Updating Vim-Tmux navigator plugin"
+	pushd "${install_dir}" > /dev/null
+	git pull -q
+	popd > /dev/null
+fi
+ok
+
