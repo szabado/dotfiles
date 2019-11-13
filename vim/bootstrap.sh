874a47eb2699659e5114018d39a89d61d2db68d8
Felix Jancso-Szabo <jancsoszabo@squareup.com>
2019-11-13 15:03:14 -0500

    vim tweaks

--- vim/bootstrap.sh
+++ vim/bootstrap.sh
@@ -24,4 +24,4 @@ autoload_plugin_dir="${plugins_base_dir}/start"
 clone_or_pull "NERDTree plugin" "https://github.com/scrooloose/nerdtree.git" "${autoload_plugin_dir}/nerdtree"
 clone_or_pull "tmux.conf plugin" "https://github.com/tmux-plugins/vim-tmux.git" "${autoload_plugin_dir}/tmux-conf-syntax"
 clone_or_pull "Vim-Tmux Navigator plugin" "https://github.com/christoomey/vim-tmux-navigator.git" "${autoload_plugin_dir}/vim-tmux-navigator"
-
+clone_or_pull "Kotlin plugin" "https://github.com/udalov/kotlin-vim.git" "${autoload_plugin_dir}/kotlin"
