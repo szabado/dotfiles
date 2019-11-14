7c4ac949bba333c442e9badef97bf51d1b852b22
Felix Jancso-Szabo <jancsoszabo@squareup.com>
2019-11-14 11:27:23 -0500

    Add git-tidy-up, and bootstrapper to install mssh

---
+++ packages/bootstrap.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+source ./helpers.sh
+
+info "installing your tools"
+
+goIsInstalled="$(command -v go)"
+
+if [[ -z "${goIsInstalled}" ]]; then
+	fail "Go is not installed, install it and re-run this script."
+else
+	running "Installing mssh"
+	go install github.com/szabado/mssh >/dev/null 2>&1; ok
+	# go install github.com/szabado/zkcli >/dev/null
+	# go install github.com/szabado/ds >/dev/null
+	rm go.{mod,sum}
+fi
