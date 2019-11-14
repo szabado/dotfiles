42aca39ff225ee600d689f90e1b095bb90ab3114
Felix Jancso-Szabo <jancsoszabo@squareup.com>
2019-11-14 15:29:56 -0500

    Comment out tools

--- packages/bootstrap.sh
+++ packages/bootstrap.sh
@@ -11,8 +11,8 @@ goIsInstalled="$(command -v go)"
 if [[ -z "${goIsInstalled}" ]]; then
 	fail "Go is not installed, install it and re-run this script."
 else
-	running "Installing mssh"
-	go install github.com/szabado/mssh >/dev/null 2>&1; ok
+	# running "Installing mssh"
+	# go install github.com/szabado/mssh >/dev/null 2>&1; ok
 	# go install github.com/szabado/zkcli >/dev/null
 	# go install github.com/szabado/ds >/dev/null
 	rm go.{mod,sum}
