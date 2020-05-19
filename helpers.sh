adbba7a946f342f9749a670f17933af4aedb3ff3
Felix Jancso-Szabo <felix.ajs@gmail.com>
2020-05-18 20:20:11 -0400

    lots of misc cleanup

--- helpers.sh
+++ helpers.sh
@@ -20,42 +20,22 @@ LIGHT_CYAN='\033[1;36m'
 WHITE='\033[1;37m'
 NC='\033[00m'
 
-fmt() {
-	if [[ -n "${1}" ]]; then
-		echo -en " ${1}"
-	fi
-	echo
-}
+ROBOT='[+]'
 
 running() {
-	echo -en " ${BLUE}┌[º.º]┐ ⇒ ${NC} ${1}..."
+	echo -en "     ⇒ ${1}..."
 }
 
 ok() {
 	echo -e "${GREEN}[ok]${NC}"
 }
 
-warn () {
-	echo -en "${YELLOW}[warn]${NC}"
-	fmt "${1}"
-}
-
-fail() {
-	echo -en "${RED}[fail]${NC}"
-	fmt "${1}"
-	exit 1
-}
-
-info() {
+log() {
 	echo -e "${LIGHT_GRAY}${1}${NC}"
 }
 
-module() {
-	echo -e " ${LIGHT_GREEN}┌[º.º]┐${NC} ${1}"
-}
-
-finished() {
-	echo -e " ${LIGHT_GREEN}┌[º.º]┐ All done! No more bare bones for you.${NC}"
+header() {
+	echo -e " ${LIGHT_GREEN}${ROBOT}${NC} ${1}"
 }
 
 user() {
@@ -63,27 +43,18 @@ user() {
 	echo
 }
 
+# Everything is tied to this to make theming changes easy if wanted
 userNoNewline() {
 	echo -en "${1}"
 }
 
-# From: http://wiki.bash-hackers.org/snipplets/print_horizontal_line
-divider() {
-	printf '%*s' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
-}
-
-backup() {
-	backupDir="${HOME}/.dotfiles/.backups"
-	mkdir -p "${backupDir}"
-
-	if [[ $# -ne 1 ]]; then
-		fail "Must specify a file to back up"
-		return 1
-	elif [[ "${1}" != /* ]]; then
-		fail "Must specify an absolute path for the file to back up"
-		return 1
+archive() {
+	if [[ $# -ne 1 || "${1}" != /* ]]; then
+		# The absolute path of a file must be specified
+		exit 1
 	fi
 
+	backupDir="${HOME}/.dotfile_backups"
 	mkdir -p "${backupDir}$(dirname ${1})"
 	if [[ -w "${1}" ]]; then
 		mv "${1}" "${backupDir}${1}"
@@ -95,18 +66,16 @@ backup() {
 lnx() {
 	target_file=${1}
 	link=${2}
-	name=$(basename "${link}")
 
 	if [[ -e ${link} ]]; then
-		backup "${link}"
+		archive "${link}"
 	fi
 
 	ln -s ${target_file} ${link}
 }
 
 prompt() {
-	user "${1}"
-	user "[y]es, [n]o"
+	user "${1} (Y/n)"
 	userNoNewline "> "
 	read -n 1 action
 	echo
@@ -126,8 +95,8 @@ err_report() {
 
 function clone_or_pull() {
 	if [[ $# -ne 3 ]]; then
-		fail "cloneOrPull has the incorrect number of arguments"
-		return 1
+		# "cloneOrPull has the incorrect number of arguments"
+		exit 1
 	fi
 
 	name="${1}"
